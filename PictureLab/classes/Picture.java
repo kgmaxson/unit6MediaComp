05811af32ba85780e4c0d93ff9f5c8e9f2f0528e
kgmaxson <kgmaxson@student.naperville203.org>
2015-01-22 15:07:22 -0600

    collage lab done

--- PictureLab/classes/Picture.java
+++ PictureLab/classes/Picture.java
@@ -98,6 +98,188 @@ public class Picture extends SimplePicture
     }
   }
   
+  public void keepOnlyBlue()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+        pixelObj.setRed(0);
+        pixelObj.setGreen(0);
+      }
+    }
+  }
+  
+  public void negate()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+        pixelObj.setRed(255-pixelObj.getRed());
+        pixelObj.setGreen(255-pixelObj.getGreen());
+        pixelObj.setBlue(255-pixelObj.getBlue());
+      }
+    }
+  }
+  
+  public void grayscale()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+        int gray = (pixelObj.getRed()+pixelObj.getGreen()+pixelObj.getBlue())/3;
+        pixelObj.setRed(gray);
+        pixelObj.setGreen(gray);
+        pixelObj.setBlue(gray);
+      }
+    }
+  }
+  
+  public void sepia()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    this.grayscale();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+        int gray = (pixelObj.getRed()+pixelObj.getGreen()+pixelObj.getBlue())/3;
+        int origRed = pixelObj.getRed();
+        int origBlue = pixelObj.getBlue();
+        int origGreen = pixelObj.getGreen();
+        if (pixelObj.getRed()<60)
+        {
+            pixelObj.setRed((int)(origRed*.85));
+            pixelObj.setGreen((int)(origGreen*.9));
+            pixelObj.setBlue((int)(origBlue*.80));
+        }
+        else if (pixelObj.getRed()<190)
+        {
+            pixelObj.setRed((int)(origRed*.9));
+            pixelObj.setGreen((int)(origGreen*.8));
+            pixelObj.setBlue((int)(origBlue*.6));
+        }
+        else
+        {
+            pixelObj.setBlue((int)(origBlue*.8));
+        }
+      }
+    }
+  }
+  
+  public void edgeDetection2(Picture sourcePicture)
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel pixel1 = null;
+    Pixel pixel2 = null;
+    for (int row = 0; row < pixels.length-1; row++)
+    {
+      for (int col = 0; col < pixels[0].length; col++)
+      {
+        pixel1 = pixels[row][col];
+        pixel2 = pixels[row+1][col];
+        int color1 = (int)pixel1.getAverage();
+        int color2 = (int)pixel2.getAverage();
+        if (color2-color1>100 || color1-color2>100)
+        {
+            pixel2.setRed(0);
+            pixel2.setBlue(0);
+            pixel2.setGreen(0);
+        }
+        else
+        {
+            pixel2.setColor(pixel1.getColor());
+        }
+      }
+    } 
+    }
+  
+  public void scaleByHalf(Picture sourcePicture)
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel leftPixel = null;
+    Pixel rightPixel = null;
+    for (int row = 0; row < pixels.length; row+=2)
+    {
+      for (int col = 0; col < pixels[0].length; col+=2)
+      {
+        leftPixel = pixels[row][col];
+        rightPixel = pixels[row/2][col/2];
+        rightPixel.setColor(leftPixel.getColor());
+      }
+    }
+    /*
+     * for (int row = 0; row < pixels.length; row++)
+    {
+      for (int col = pixels[0].length/2; col < pixels[0].length; col++)
+      {
+        leftPixel = pixels[35][115];
+        rightPixel = pixels[row][col];
+        rightPixel.setColor(leftPixel.getColor());
+      }
+    } 
+    
+     for (int row = pixels.length/2; row < pixels.length; row++)
+    {
+      for (int col = 0; col < pixels[0].length; col++)
+      {
+        leftPixel = pixels[35][115];
+        rightPixel = pixels[row][col];
+        rightPixel.setColor(leftPixel.getColor());
+      }
+    } 
+    */
+    }
+  
+  public void fixUnderwater()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+      int red = pixelObj.getRed();
+      int green = pixelObj.getGreen();
+      int blue = pixelObj.getBlue();
+      if (red<50){red=120;}
+      if (green>blue){red+=40;}
+      if (green<50){green=150;}
+      if (blue<50){blue=120;}
+        pixelObj.setRed((int)((red-120)*2.05));
+        pixelObj.setGreen((int)((green-150)*2.05));
+        pixelObj.setBlue((int)((blue-100)*2.05));
+      }
+    }
+  }
+  
+  public void fixUnderwater2()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+      int red = pixelObj.getRed();
+      int green = pixelObj.getGreen();
+      int blue = pixelObj.getBlue();
+      if (red<30){red=50;}
+      if (green<30){green=50;}
+      if (blue<30){blue=60;}
+      else if (blue>160 && green<170){red=190;}
+      if ((green-60)*1.45>55&&red<90){green=0;blue=0;red=0;}
+        pixelObj.setRed((int)((red-0)*12.45));
+        pixelObj.setGreen((int)((green-60)*12.45));
+        pixelObj.setBlue((int)((blue-90)*12.45));
+      
+      }
+    }
+  }
+  
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
     * from left to right */
@@ -176,6 +358,54 @@ public class Picture extends SimplePicture
     } 
   }
   
+  /** Method that mirrors the picture around a 
+    * horizontal mirror in the center of the picture */
+  public void mirrorArms()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel topPixel = null;
+    Pixel bottomPixel = null;
+    int width = pixels[0].length;
+    for (int row = 155; row < 196; row++)
+    {
+      for (int col = 101; col < 171; col++)
+      {
+        topPixel = pixels[row][col];
+        bottomPixel = pixels[pixels.length-row+80][col];
+        bottomPixel.setColor(topPixel.getColor());
+      }
+    }
+    
+    for (int row = 166; row < 195; row++)
+    {
+      for (int col = 238; col < 293; col++)
+      {
+        topPixel = pixels[row][col];
+        bottomPixel = pixels[pixels.length-row+88][col];
+        bottomPixel.setColor(topPixel.getColor());
+      }
+    }
+  }
+  
+  /** Method that mirrors the picture around a 
+    * horizontal mirror in the center of the picture */
+  public void mirrorGull()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel leftPixel = null;
+    Pixel rightPixel = null;
+    int width = pixels[0].length;
+    for (int row = 230; row < 328; row++)
+    {
+      for (int col = 230; col < 345; col++)
+      {
+        leftPixel = pixels[row][col];
+        rightPixel = pixels[row][700-col];
+        rightPixel.setColor(leftPixel.getColor());
+      }
+    }
+  }
+  
   /** Mirror just part of a picture of a temple */
   public void mirrorTemple()
   {
@@ -199,6 +429,30 @@ public class Picture extends SimplePicture
       }
     }
   }
+  
+  void cropAndCopy( Picture sourcePicture, int startSourceRow, int endSourceRow, int startSourceCol, int endSourceCol,
+         int startDestRow, int startDestCol )
+         {
+    Pixel fromPixel = null;
+    Pixel toPixel = null;
+    Pixel[][] toPixels = this.getPixels2D();
+    Pixel[][] fromPixels = sourcePicture.getPixels2D();
+    for (int fromRow = startSourceRow, toRow = startDestRow; 
+         fromRow < endSourceRow &&
+         toRow < startDestRow+(endSourceRow-startSourceRow); 
+         fromRow++, toRow++)
+    {
+      for (int fromCol = startSourceCol, toCol = startDestCol; 
+           fromCol < endSourceCol &&
+           toCol < startDestCol+(endSourceCol-startSourceCol);  
+           fromCol++, toCol++)
+      {
+        fromPixel = fromPixels[fromRow][fromCol];
+        toPixel = toPixels[toRow][toCol];
+        toPixel.setColor(fromPixel.getColor());
+      }
+    }       
+            }
   
   /** copy from the passed fromPic to the
     * specified startRow and startCol in the
@@ -234,18 +488,66 @@ public class Picture extends SimplePicture
   /** Method to create a collage of several pictures */
   public void createCollage()
   {
-    Picture flower1 = new Picture("flower1.jpg");
-    Picture flower2 = new Picture("flower2.jpg");
-    this.copy(flower1,0,0);
-    this.copy(flower2,100,0);
-    this.copy(flower1,200,0);
-    Picture flowerNoBlue = new Picture(flower2);
-    flowerNoBlue.zeroBlue();
-    this.copy(flowerNoBlue,300,0);
-    this.copy(flower1,400,0);
-    this.copy(flower2,500,0);
-    this.mirrorVertical();
-    this.write("collage.jpg");
+    Picture davis1 = new Picture("davis1.jpg");
+    Picture davis2 = new Picture("davis2.jpg");
+    Picture davis3 = new Picture("davis3.jpg");
+    Picture davis4 = new Picture("davis4.jpg");
+    Picture davis5 = new Picture("davis5.jpg");
+    
+    Picture davisGray = new Picture(davis1);
+    davisGray.grayscale();
+    this.copy(davisGray,0,0);
+    
+    Picture davisMirrorRightLeft = new Picture(davis4);
+    davisMirrorRightLeft.mirrorRightToLeft();
+    davisMirrorRightLeft.scaleByHalf(davisMirrorRightLeft);
+    davisMirrorRightLeft.scaleByHalf(davisMirrorRightLeft);
+    this.cropAndCopy(davisMirrorRightLeft,0,239,0,179,340,0);
+    
+    Picture davisSepia = new Picture(davis2);
+    davisSepia.scaleByHalf(davisSepia);
+    davisSepia.scaleByHalf(davisSepia);
+    davisSepia.sepia();
+    this.cropAndCopy(davisSepia,0,179,0,119,350,600);
+    
+    Picture davisTopLeft = new Picture(davis1);
+    davisTopLeft.scaleByHalf(davisTopLeft);
+    davisTopLeft.scaleByHalf(davisTopLeft);
+    davisTopLeft.negate();
+    this.cropAndCopy(davisTopLeft,0,239,0,179,0,0);
+    
+    Picture davisTopRight = new Picture(davis1);
+    davisTopRight.scaleByHalf(davisTopRight);
+    davisTopRight.scaleByHalf(davisTopRight);
+    davisTopRight.edgeDetection(16);
+    this.cropAndCopy(davisTopRight,0,239,0,179,0,540);
+    
+    Picture davisBotLeft = new Picture(davis1);
+    davisBotLeft.scaleByHalf(davisBotLeft);
+    davisBotLeft.scaleByHalf(davisBotLeft);
+    davisBotLeft.zeroBlue();
+    this.cropAndCopy(davisBotLeft,0,239,0,179,640,0);
+    
+    Picture davisBotRight = new Picture(davis1);
+    davisBotRight.scaleByHalf(davisBotRight);
+    davisBotRight.scaleByHalf(davisBotRight);
+    davisBotRight.fixUnderwater();
+    this.cropAndCopy(davisBotRight,0,239,0,179,640,540);
+    
+    Picture davisEdgeDetection = new Picture(davis5);
+    davisEdgeDetection.scaleByHalf(davisEdgeDetection);
+    davisEdgeDetection.sepia();
+    this.cropAndCopy(davisEdgeDetection,0,207,0,311,305,230);
+    
+    Picture davisMirrorPool = new Picture(davis3);
+    davisMirrorPool.mirrorBotToTop();
+    davisMirrorPool.scaleByHalf(davisMirrorPool);
+    davisMirrorPool.scaleByHalf(davisMirrorPool);
+    this.cropAndCopy(davisMirrorPool,0,103,0,157,774,305);
+    
+    
+    
+    this.write("finalCollage.jpg");
   }
   
   
@@ -268,9 +570,15 @@ public class Picture extends SimplePicture
         rightColor = rightPixel.getColor();
         if (leftPixel.colorDistance(rightColor) > 
             edgeDist)
-          leftPixel.setColor(Color.BLACK);
+            {
+          leftPixel.setColor(Color.GREEN);
+        }
         else
-          leftPixel.setColor(Color.WHITE);
+        {
+          leftPixel.setBlue(0);
+          leftPixel.setGreen(00);
+          leftPixel.setRed(0);
+        }
       }
     }
   }
--- PictureLab/classes/PictureTester.java
+++ PictureLab/classes/PictureTester.java
@@ -17,6 +17,62 @@ public class PictureTester
     beach.explore();
   }
   
+  public static void testKeepOnlyBlue()
+  {
+    Picture beach = new Picture("davis1.jpg");
+    beach.explore();
+    beach.keepOnlyBlue();
+    beach.explore();
+  }
+  
+  public static void testNegate()
+  {
+    Picture davis = new Picture("davis1.jpg");
+    davis.explore();
+    davis.negate();
+    davis.explore();
+  }
+  
+  public static void testGrayscale()
+  {
+    Picture davis = new Picture("davis1.jpg");
+    davis.explore();
+    davis.grayscale();
+    davis.explore();
+  }
+  
+  public static void testFixUnderwater()
+  {
+    Picture water = new Picture("water.jpg");
+    water.explore();
+    water.fixUnderwater();
+    water.explore();
+  }
+  
+  public static void testFixUnderwater2()
+  {
+    Picture water = new Picture("water.jpg");
+    water.explore();
+    water.fixUnderwater2();
+    water.explore();
+  }
+  
+  public static void testScaleByHalf()
+  {
+    Picture davis = new Picture("davis1.jpg");
+    davis.explore();
+    davis.scaleByHalf(davis);
+    davis.explore();
+  }
+  
+  public static void testEdgeDetection2()
+  {
+    Picture davis = new Picture("davis1.jpg");
+    davis.explore();
+    davis.edgeDetection2(davis);
+    davis.explore();
+  }
+  
   /** Method to test mirrorVertical */
   public static void testMirrorVertical()
   {
@@ -49,7 +105,23 @@ public class PictureTester
       davis1.mirrorBotToTop();
       davis1.explore();
     }
-  
+    
+  public static void testMirrorArms()
+  {
+      Picture snowman = new Picture("snowman.jpg");
+      snowman.explore();
+      snowman.mirrorArms();
+      snowman.explore();
+    }
+ 
+  public static void testMirrorGull()
+  {
+      Picture seagull = new Picture("seagull.jpg");
+      seagull.explore();
+      seagull.mirrorGull();
+      seagull.explore();
+    }
+    
   /** Method to test mirrorTemple */
   public static void testMirrorTemple()
   {
@@ -62,17 +134,42 @@ public class PictureTester
   /** Method to test the collage method */
   public static void testCollage()
   {
-    Picture canvas = new Picture("640x480.jpg");
+    Picture canvas = new Picture(880,720);
+    Picture davis1 = new Picture("davis1.jpg");
+    Picture davis2 = new Picture("davis2.jpg");
+    Picture davis3 = new Picture("davis3.jpg");
+    Picture davis4 = new Picture("davis4.jpg");
+    Picture davis5 = new Picture("davis5.jpg");
+    //davis1.explore();
+    //davis2.explore();
+    //davis3.explore();
+    //davis4.explore();
+    //davis5.explore();
     canvas.createCollage();
     canvas.explore();
+  }
+  
+  public static void testCropAndCopy()
+  {
+    Picture davis = new Picture("davis1.jpg");
+    Picture seagull = new Picture("seagull.jpg");
+    Picture snowman = new Picture("snowman.jpg");
+    davis.explore();
+    davis.cropAndCopy(seagull, 234, 330, 232, 338, 622, 349);
+    davis.cropAndCopy(snowman, 81, 300, 155, 250, 267, 50);
+    davis.explore();
+    // copies the region defined by rows 10-100 and columns 20-200 of picture1 into picture2
+    // such that the upper-left corner of the copied picture starts at row 30 and column 40
+    //(and, therefore, extends to row 120 and column 220).
   }
   
   /** Method to test edgeDetection */
   public static void testEdgeDetection()
   {
-    Picture swan = new Picture("swan.jpg");
-    swan.edgeDetection(10);
-    swan.explore();
+    Picture davis = new Picture("davis1.jpg");
+    davis.explore();
+    davis.edgeDetection(13);
+    davis.explore();
   }
   
   /** Main method for testing.  Every class can have a main
@@ -92,12 +189,21 @@ public class PictureTester
     //testMirrorVertical();
     //testMirrorRightToLeft();
     //testMirrorHorizontal();
-    testMirrorBotToTop();
+    //testMirrorBotToTop();
+    //testMirrorArms();
+    //testMirrorGull();
+    //testKeepOnlyBlue();
+    //testNegate();
+    //testGrayscale();
+    //testFixUnderwater2();
+    //testCropAndCopy();
+    //testScaleByHalf();
+    //testEdgeDetection2();
     //testMirrorTemple();
     //testMirrorArms();
     //testMirrorGull();
     //testMirrorDiagonal();
-    //testCollage();
+    testCollage();
     //testCopy();
     //testEdgeDetection();
     //testEdgeDetection2();
